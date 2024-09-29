62f84d4df2828abae9bddb863c8745c1927eb004
jcaraballo <jcaraballo@users.noreply.github.com>
2024-09-29 18:28:43 +0100

    Docker (root) installation: Update to latest instructions
    Including adding installation of docker-buildx-plugin package

--- boot/701-as-root-install-docker-ce-and-docker-compose.bash
+++ boot/701-as-root-install-docker-ce-and-docker-compose.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 echo 'Installing dependencies and key' >&2
-apt-get install ca-certificates curl gnupg lsb-release && \
-mkdir -p /etc/apt/keyrings && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+apt install ca-certificates curl gnupg && \
+  install -m 0755 -d /etc/apt/keyrings && \
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && \
+  chmod a+r /etc/apt/keyrings/docker.asc
 
 echo 'Adding repository and installing' >&2
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list && \
-apt-get update && \
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" > /etc/apt/sources.list.d/docker.list && \
+  apt update && \
+  apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
