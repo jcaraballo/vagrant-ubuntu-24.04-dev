2bc639db4eb995eb04b0eb3034cf4a94606a02ef
jcaraballo <jcaraballo@users.noreply.github.com>
2025-01-19 11:55:38 +0000

    Upgrade JDK to openjdk-21

--- boot/302-as-root-install-openjdk.bash
+++ boot/302-as-root-install-openjdk.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'Installing OpenJDK 18'
-apt-get install -y openjdk-17-jdk openjdk-17-source
+echo 'Installing OpenJDK 21'
+apt-get install -y openjdk-21-jdk openjdk-21-source
 
